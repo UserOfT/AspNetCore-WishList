e98bf786fb9eb50b36efba627ecd2304a22d084f
ahmed.layachi <ahmed.layachi@outlook.com>
2019-12-14 00:06:22 +0100

    Startup fixed

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -16,9 +16,14 @@ namespace WishList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app = env.IsDevelopment()
-                  ? app.UseDeveloperExceptionPage()
-                  : app.UseExceptionHandler("/Home/Error");
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
 
             app.UseMvcWithDefaultRoute();
         }
